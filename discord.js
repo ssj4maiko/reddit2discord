cb4666a047aafd3e6d7002425633312472e39505
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2021-08-03 11:02:58 -0300

    Isolating colors, and ignoring the new file, so others can change it without much concern

--- discord.js
+++ discord.js
@@ -1,23 +1,9 @@
 var htmlEntities = require('html-entities');
 var request = require('request');
+var FlairColors = require('./flair-colors.js');
 
 module.exports = class Discord {
-	// https://gist.github.com/thomasbnt/b6f455e2c7d743b796917fa3c205f812#gistcomment-3656937 for colors, End of file for the array
-	// Set CSS rules for the flairs
-	postFlairs = {
-		'post-flair-translated'		:	COLORS.BLUE,
-		'post-flair-untranslated'	:	COLORS.RED,
-		'post-flair-manga'			:	COLORS.WHITE,
-		'post-flair-fanart'			:	COLORS.AQUA,
-		'post-flair-illustration'	:	COLORS.GREEN,
-		'post-flair-meme'			:	COLORS.GREY,
-		'post-flair-meta'			:	COLORS.GOLD,
-		'post-flair-announcement'	:	COLORS.PURPLE,
-		'user-flair-raw'			:	COLORS.RED,
-		'user-flair-wn'				:	COLORS.BLUE,
-		'user-flair-manga'			:	COLORS.LIGHT_GREY,
-		'user-flair-ln'				:	COLORS.DARK_PURPLE
-	};
+	postFlairs = FlairColors.FLAIRS;
 	webhook = {
 		post : null,
 		comment : null
@@ -78,7 +64,7 @@ module.exports = class Discord {
 					"embeds": [{
 						"title" : htmlEntities.decode((data.link_flair_text ? '['+data.link_flair_text+'] ' : '')+(data.title || data.link_title)),
 						"url" : htmlEntities.decode(data.url || data.link_url),
-						"color"	: this.postFlairs[data.link_flair_css_class] || COLORS.WHITE,
+						"color"	: this.postFlairs[data.link_flair_css_class] || FlairColors.COLORS.WHITE,
 						"author": {
 							"name": htmlEntities.decode(data.author_flair_text ? '['+data.author_flair_text+'] ' : '')+"/u/"+data.author,
 							//"icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
@@ -205,7 +191,7 @@ module.exports = class Discord {
 					"title" : '- '+(data.link_flair_text ? '['+htmlEntities.decode(data.link_flair_text)+'] ' : '')
 								+htmlEntities.decode(data.link_title),
 					"url" : htmlEntities.decode(data.link_permalink+data.id),
-					"color"	: this.postFlairs[data.author_flair_css_class] || COLORS.ORANGE,
+					"color"	: this.postFlairs[data.author_flair_css_class] || FlairColors.COLORS.ORANGE,
 					"author": {
 						"name": "/u/"+data.author+' '+htmlEntities.decode(data.author_flair_text ? '['+data.author_flair_text+'] ' : ''),
 						//"icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
@@ -233,34 +219,3 @@ module.exports = class Discord {
 		});
 	}
 }
-const COLORS = {
-	'DEFAULT'				:	0,			// 	#000000
-	'AQUA'					:	1752220,	// 	#1ABC9C
-	'DARK_AQUA'				:	1146986,	// 	#11806A
-	'GREEN'					:	3066993,	// 	#2ECC71
-	'DARK_GREEN'			:	2067276,	// 	#1F8B4C
-	'BLUE'					:	3447003,	// 	#3498DB
-	'DARK_BLUE'				:	2123412,	// 	#206694
-	'PURPLE'				:	10181046,	// 	#9B59B6
-	'DARK_PURPLE'			:	7419530,	// 	#71368A
-	'LUMINOUS_VIVID_PINK'	:	15277667,	// 	#E91E63
-	'DARK_VIVID_PINK'		:	11342935,	// 	#AD1457
-	'GOLD'					:	15844367,	// 	#F1C40F
-	'DARK_GOLD'				:	12745742,	// 	#C27C0E
-	'ORANGE'				:	15105570,	// 	#E67E22
-	'DARK_ORANGE'			:	11027200,	// 	#A84300
-	'RED'					:	15158332,	// 	#E74C3C
-	'DARK_RED'				:	10038562,	// 	#992D22
-	'GREY'					:	9807270,	// 	#95A5A6
-	'DARK_GREY'				:	9936031,	// 	#979C9F
-	'DARKER_GREY'			:	8359053,	// 	#7F8C8D
-	'LIGHT_GREY'			:	12370112,	// 	#BCC0C0
-	'NAVY'					:	3426654,	// 	#34495E
-	'DARK_NAVY'				:	2899536,	// 	#2C3E50
-	'YELLOW'				:	16776960,	// 	#FFFF00
-	'WHITE'					:	16777215,	// 	#FFFFFF
-	'BLURPLE'				:	7506394,	// 	#7289DA
-	'GREYPLE'				:	10070709,	//	#99AAB5
-	'DARK_BUT_NOT_BLACK'	:	2895667,	// 	#2C2F33
-	'NOT_QUITE_BLACK'		:	2303786,	//	#23272A
-};
