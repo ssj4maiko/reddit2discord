8739ff12139b401a40cb4f9e2b2d6c081f5d0e8f
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2022-04-29 10:02:17 -0300

    Catch on watcher

--- src/reddit/watcher.ts
+++ src/reddit/watcher.ts
@@ -37,6 +37,21 @@ export default class Watcher {
 		//console.log('checking again for ' + this.typeName +'S');
 		let response = await axios.get(this.URL, {
 			headers: this.headers
+		}).catch((error) => {
+			if (error.response) {
+				// The request was made and the server responded with a status code
+				// that falls out of the range of 2xx
+				return error.response;
+			} else if (error.request) {
+				// The request was made but no response was received
+				// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+				// http.ClientRequest in node.js
+				return error.request;
+			} else {
+				// Something happened in setting up the request that triggered an Error
+				console.log('Error', error.message);
+			}
+			console.log(error.config);
 		}),
 			lastIndex:number = 9;
 		if (response.status < 200 || response.status >= 300) {
